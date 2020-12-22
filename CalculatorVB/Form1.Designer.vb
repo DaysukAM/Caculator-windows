b8ec99ced51b692e5066647cb5d2562f415805dc
DaysukAM <anthony.moutonnet@gmail.com>
2020-12-22 14:28:54 +0100

    project rebuild and V1.1

---
+++ CalculatorVB/Form1.Designer.vb
@@ -0,0 +1,238 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.ButtonPlus = New System.Windows.Forms.Button()
+        Me.ButtonMin = New System.Windows.Forms.Button()
+        Me.ButtonClear = New System.Windows.Forms.Button()
+        Me.ButtonDiv = New System.Windows.Forms.Button()
+        Me.ButtonMult = New System.Windows.Forms.Button()
+        Me.ButtonResult = New System.Windows.Forms.Button()
+        Me.ButtonPoint = New System.Windows.Forms.Button()
+        Me.ButtonAllClear = New System.Windows.Forms.Button()
+        Me.ButtonAns = New System.Windows.Forms.Button()
+        Me.ResultBox = New System.Windows.Forms.Label()
+        Me.CalcBox = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        resources.ApplyResources(Me.Button1, "Button1")
+        Me.Button1.Name = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        resources.ApplyResources(Me.Button2, "Button2")
+        Me.Button2.Name = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        resources.ApplyResources(Me.Button3, "Button3")
+        Me.Button3.Name = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        resources.ApplyResources(Me.Button4, "Button4")
+        Me.Button4.Name = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        resources.ApplyResources(Me.Button5, "Button5")
+        Me.Button5.Name = "Button5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        resources.ApplyResources(Me.Button6, "Button6")
+        Me.Button6.Name = "Button6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        resources.ApplyResources(Me.Button7, "Button7")
+        Me.Button7.Name = "Button7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        resources.ApplyResources(Me.Button8, "Button8")
+        Me.Button8.Name = "Button8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        resources.ApplyResources(Me.Button9, "Button9")
+        Me.Button9.Name = "Button9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button0
+        '
+        resources.ApplyResources(Me.Button0, "Button0")
+        Me.Button0.Name = "Button0"
+        Me.Button0.UseVisualStyleBackColor = True
+        '
+        'ButtonPlus
+        '
+        Me.ButtonPlus.BackColor = System.Drawing.Color.Gray
+        resources.ApplyResources(Me.ButtonPlus, "ButtonPlus")
+        Me.ButtonPlus.Name = "ButtonPlus"
+        Me.ButtonPlus.UseVisualStyleBackColor = False
+        '
+        'ButtonMin
+        '
+        Me.ButtonMin.BackColor = System.Drawing.Color.Gray
+        resources.ApplyResources(Me.ButtonMin, "ButtonMin")
+        Me.ButtonMin.Name = "ButtonMin"
+        Me.ButtonMin.UseVisualStyleBackColor = False
+        '
+        'ButtonClear
+        '
+        Me.ButtonClear.BackColor = System.Drawing.Color.DarkRed
+        resources.ApplyResources(Me.ButtonClear, "ButtonClear")
+        Me.ButtonClear.Name = "ButtonClear"
+        Me.ButtonClear.UseVisualStyleBackColor = False
+        '
+        'ButtonDiv
+        '
+        Me.ButtonDiv.BackColor = System.Drawing.Color.Gray
+        resources.ApplyResources(Me.ButtonDiv, "ButtonDiv")
+        Me.ButtonDiv.Name = "ButtonDiv"
+        Me.ButtonDiv.UseVisualStyleBackColor = False
+        '
+        'ButtonMult
+        '
+        Me.ButtonMult.BackColor = System.Drawing.Color.Gray
+        resources.ApplyResources(Me.ButtonMult, "ButtonMult")
+        Me.ButtonMult.Name = "ButtonMult"
+        Me.ButtonMult.UseVisualStyleBackColor = False
+        '
+        'ButtonResult
+        '
+        Me.ButtonResult.BackColor = System.Drawing.SystemColors.ControlLightLight
+        resources.ApplyResources(Me.ButtonResult, "ButtonResult")
+        Me.ButtonResult.Name = "ButtonResult"
+        Me.ButtonResult.UseVisualStyleBackColor = False
+        '
+        'ButtonPoint
+        '
+        resources.ApplyResources(Me.ButtonPoint, "ButtonPoint")
+        Me.ButtonPoint.Name = "ButtonPoint"
+        Me.ButtonPoint.UseVisualStyleBackColor = True
+        '
+        'ButtonAllClear
+        '
+        Me.ButtonAllClear.BackColor = System.Drawing.Color.Red
+        resources.ApplyResources(Me.ButtonAllClear, "ButtonAllClear")
+        Me.ButtonAllClear.Name = "ButtonAllClear"
+        Me.ButtonAllClear.UseVisualStyleBackColor = False
+        '
+        'ButtonAns
+        '
+        resources.ApplyResources(Me.ButtonAns, "ButtonAns")
+        Me.ButtonAns.Name = "ButtonAns"
+        Me.ButtonAns.UseVisualStyleBackColor = True
+        '
+        'ResultBox
+        '
+        Me.ResultBox.BackColor = System.Drawing.Color.Black
+        Me.ResultBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        resources.ApplyResources(Me.ResultBox, "ResultBox")
+        Me.ResultBox.ForeColor = System.Drawing.Color.White
+        Me.ResultBox.Name = "ResultBox"
+        '
+        'CalcBox
+        '
+        Me.CalcBox.BackColor = System.Drawing.Color.Black
+        Me.CalcBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        resources.ApplyResources(Me.CalcBox, "CalcBox")
+        Me.CalcBox.ForeColor = System.Drawing.Color.White
+        Me.CalcBox.Name = "CalcBox"
+        '
+        'Form1
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.CalcBox)
+        Me.Controls.Add(Me.ResultBox)
+        Me.Controls.Add(Me.ButtonAns)
+        Me.Controls.Add(Me.ButtonAllClear)
+        Me.Controls.Add(Me.ButtonPoint)
+        Me.Controls.Add(Me.ButtonResult)
+        Me.Controls.Add(Me.ButtonMult)
+        Me.Controls.Add(Me.ButtonDiv)
+        Me.Controls.Add(Me.ButtonClear)
+        Me.Controls.Add(Me.ButtonMin)
+        Me.Controls.Add(Me.ButtonPlus)
+        Me.Controls.Add(Me.Button0)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button0 As Button
+    Friend WithEvents ButtonPlus As Button
+    Friend WithEvents ButtonMin As Button
+    Friend WithEvents ButtonClear As Button
+    Friend WithEvents ButtonDiv As Button
+    Friend WithEvents ButtonMult As Button
+    Friend WithEvents ButtonResult As Button
+    Friend WithEvents ButtonPoint As Button
+    Friend WithEvents ButtonAllClear As Button
+    Friend WithEvents ButtonAns As Button
+    Friend WithEvents CalcBox As Label
+    Friend WithEvents Calc As Label
+    Friend WithEvents ResultBox As Label
+End Class
---
+++ CalculatorVB/Form1.vb
@@ -0,0 +1,250 @@
+﻿Public Class Form1
+    'Définission de result Afin d'afficher le résultat et s'en servir pour le bouton Ans (Answer)
+    Dim result As Double
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub CalcBox_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+    Private Sub ResultBox_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        CalcBox.Text = CalcBox.Text + "1"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        CalcBox.Text = CalcBox.Text + "2"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        CalcBox.Text = CalcBox.Text + "3"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        CalcBox.Text = CalcBox.Text + "4"
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        CalcBox.Text = CalcBox.Text + "5"
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        CalcBox.Text = CalcBox.Text + "6"
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        CalcBox.Text = CalcBox.Text + "7"
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        CalcBox.Text = CalcBox.Text + "8"
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        CalcBox.Text = CalcBox.Text + "9"
+    End Sub
+
+    Private Sub Button0_Click(sender As Object, e As EventArgs) Handles Button0.Click
+        CalcBox.Text = CalcBox.Text + "0"
+    End Sub
+
+    Private Sub ButtonPoint_Click(sender As Object, e As EventArgs) Handles ButtonPoint.Click
+        CalcBox.Text = CalcBox.Text + ","
+    End Sub
+
+    'Bouton Ans (Answer) qui permet à l'utilisateur d'écrire la réponse dans le calcul (Arrondi au millième près)
+    Private Sub ButtonAns_Click(sender As Object, e As EventArgs) Handles ButtonAns.Click
+        If result = vbEmpty Then
+        Else
+            result = Math.Round(result, 3)
+            CalcBox.Text = CalcBox.Text + CStr(result)
+        End If
+    End Sub
+
+    Private Sub ButtonPlus_Click(sender As Object, e As EventArgs) Handles ButtonPlus.Click
+        CalcBox.Text = CalcBox.Text + "+"
+    End Sub
+
+    Private Sub ButtonMin_Click(sender As Object, e As EventArgs) Handles ButtonMin.Click
+        CalcBox.Text = CalcBox.Text + "-"
+    End Sub
+
+    Private Sub ButtonMult_Click(sender As Object, e As EventArgs) Handles ButtonMult.Click
+        CalcBox.Text = CalcBox.Text + "x"
+    End Sub
+
+    Private Sub ButtonDiv_Click(sender As Object, e As EventArgs) Handles ButtonDiv.Click
+        CalcBox.Text = CalcBox.Text + "/"
+    End Sub
+
+    Private Sub ButtonResult_Click(sender As Object, e As EventArgs) Handles ButtonResult.Click
+
+        If CStr(Calcul(CalcBox.Text)) = "Error" Then
+        Else
+            result = Calcul(CalcBox.Text)
+        End If
+        ResultBox.Text = Calcul(CalcBox.Text)
+        CalcBox.Text = ""
+    End Sub
+
+    Private Sub ButtonAllClear_Click(sender As Object, e As EventArgs) Handles ButtonAllClear.Click
+        CalcBox.Text = ""
+    End Sub
+
+    Private Sub ButtonClear_Click(sender As Object, e As EventArgs) Handles ButtonClear.Click
+        If CalcBox.Text.Length > 0 Then
+            CalcBox.Text = CalcBox.Text.Remove(CalcBox.Text.Length - 1, 1)
+        End If
+    End Sub
+
+    'Fonction de Calcul et de vérification d'erreur
+    Private Function Calcul(CalcString As String)
+        '1-Algorythme de vérificationet séparation
+        Dim CalcArray() As Char = CalcString
+        Dim nb(2) As String
+        'y est un compteur pour le nombre
+        Dim y As Integer = 0
+        'x est un compteur pour l'opérateur
+        Dim x As Integer = 0
+        Dim op(1) As String
+        'verification que le champ n'est pas vide
+        If CalcString = "" Then
+            Return 0
+        End If
+        'verification premier et dernier caractère
+        If IsNumeric(CalcArray(0)) And IsNumeric(CalcArray(CalcArray.Length - 1)) Then
+        Else
+            Return "Error"
+        End If
+        'Boucle de parcours de la chaine de caractère 
+
+        For i As Integer = 0 To CalcArray.Length - 1
+
+            'verification du nombre
+            If IsNumeric(CalcArray(i)) Then
+                nb(y) = nb(y) & CalcArray(i)
+            Else
+                'système de verification des , pour nombre décimal
+                If CalcArray(i) = "," Then
+                    nb(y) = nb(y) & CalcArray(i)
+                    'verification du caractère juste après la ,
+                    If i + 1 >= CalcArray.Length Then
+                        Return "Error"
+                    ElseIf IsNumeric(CalcArray(i + 1)) Then
+                    Else
+                        Return "Error"
+                    End If
+                Else
+                    'verifiction du 1er opérateur
+                    Select Case CalcArray(i)
+                        Case "+"
+                            op(x) = "+"
+                        Case "-"
+                            op(x) = "-"
+                        Case "x"
+                            op(x) = "x"
+                        Case "/"
+                            op(x) = "/"
+                    End Select
+                    'verification du caractère juste après l'opérateur
+                    If i + 1 >= CalcArray.Length Then
+                        Return "Error"
+                    ElseIf IsNumeric(CalcArray(i + 1)) Then
+                        x = x + 1
+                    Else
+                        Return "Error"
+                    End If
+                    y = y + 1
+                    nb(y) = ""
+
+                End If
+            End If
+
+        Next
+        'Vérification pour cas de nombre à plusieurs virgules
+        For i As Integer = 0 To nb.Length - 1
+            If (Len(nb(i)) - Len(Replace(nb(i), ",", "", , , 0))) / Len(",") > 1 Then
+                Return "Error"
+            End If
+        Next
+        '2-Algorythme de calcul en fonction de l'opérateur
+        'Redefinition des 3 nombres en Double (nbTemp ets le resultat du premier calcul)
+        Dim nb0, nb1, nb2, nbTemp As Double
+        nb0 = CDbl(nb(0))
+        nb1 = CDbl(nb(1))
+        nb2 = CDbl(nb(2))
+        'verification qu'il y a bien un opérateur
+        If op(0) = "" Then
+            Return nb0
+        End If
+        'verification de la priorité opératoire
+        If op(1) = "x" Or op(1) = "/" Then
+            'Calcul quand le 2eme opérateur est prioritaire
+            Select Case op(1)
+                Case "x"
+                    'gestion du cas exeptionnel de la division en premier
+                    If op(0) = "/" Then
+                        nbTemp = nb0 / nb1
+                        result = nbTemp * nb2
+                        Return result
+                    Else
+                        nbTemp = nb1 * nb2
+                    End If
+
+                Case "/"
+                    'gestion du cas exeptionnel des doubles divisions
+                    If op(0) = "/" Then
+                        nbTemp = nb0 / nb1
+                        result = nbTemp / nb2
+                        Return result
+                    Else
+                        nbTemp = nb1 / nb2
+                    End If
+            End Select
+            Select Case op(0)
+                Case "+"
+                    result = nb0 + nbTemp
+                    Return result
+                Case "-"
+                    result = nb0 - nbTemp
+                    Return result
+                Case "x"
+                    result = nb0 * nbTemp
+                    Return result
+            End Select
+        Else
+            'Calcul quand le 1eme opérateur est prioritaire (ou quand il n'y a pas de 2eme opérateur)
+            Select Case op(0)
+                Case "+"
+                    nbTemp = nb0 + nb1
+                Case "-"
+                    nbTemp = nb0 - nb1
+                Case "x"
+                    nbTemp = nb0 * nb1
+                Case "/"
+                    nbTemp = nb0 / nb1
+
+            End Select
+            Select Case op(1)
+                Case "+"
+                    result = nbTemp + nb2
+                    Return result
+                Case "-"
+                    result = nbTemp - nb2
+                    Return result
+            End Select
+
+        End If
+        'si il n'y a pas de 2eme opérateur, la fonction renvoie nbTemp car il correspond au resultat du calcul simple à un opérateur
+        Return nbTemp
+    End Function
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles  ResultBox.Click
+
+    End Sub
+End Class
